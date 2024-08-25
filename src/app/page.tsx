23eca0ff1d7bcf608ae620f5ffd1447b93711827
関口峻矢 <sekiguchishunya0619@gmail.com>
2024-08-26 02:21:43 +0900

    k

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,10 @@ import Script from 'next/script';
 import DeviationScoreCalculator from './components/DeviationScoreCalculator';
 import MotivationalQuote from './components/MotivationalQuote';
 import { Container, Grid } from '@mui/material';
-import Head from 'next/head'
 
 const Home: React.FC = () => {
   return (
     <>
-      <Head>
-        <title>偏差値予測計算機</title>
-        <link rel="icon" href="/favicon.jpg" />
-        <meta property="og:title" content="偏差値予測計算機" />
-        <meta property="og:description" content="今の偏差値と勉強時間だけで未来の偏差値とオススメの参考書を提示してくれるサイト" />
-        <meta property="og:image" content="https://study-time-32om.vercel.app/favicon.jpg" />
-        <meta property="og:url" content="https://study-time-32om.vercel.app/" />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5463240386609584"
